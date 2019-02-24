1df11af76010b41a1f6ee2a643dcebcd2a2728f2
TiffanyJan <tiffany_low@hotmail.com>
2019-02-24 22:33:31 +0000

    insert basic array to return course materials

--- src/viewcomponents/CourseMaterialsView.js
+++ src/viewcomponents/CourseMaterialsView.js
@@ -2,13 +2,27 @@ import React from 'react'
 import './CourseMaterialsView.css'
 
 export default class CourseMaterialsView extends React.Component{
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            courseMaterials: [
+                "JavaScript",
+                "HTML",
+                "React",
+                "Tutorials"
+            ]
+        }
+    }  
+
     render(){
         return(
            <div className="container-fluid">
                 <div className="row">
                     <div className="col-12">
-                        <p>This is the course materials view</p>
-                    </div>
+                        <h1>Course Materials</h1> 
+                    {this.state.courseMaterials.map(materials => <h1>{materials}</h1>) }
+                   </div>
                 </div>
            </div>
         )
