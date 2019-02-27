760a2e80a7bc237cf145d5cddca880d65af961a3
TiffanyJan <tiffany_low@hotmail.com>
2019-02-27 23:31:32 +0000

    inserted resources for topics, open/close buttons displaying content

--- src/viewcomponents/CourseMaterialsView.js
+++ src/viewcomponents/CourseMaterialsView.js
@@ -1,41 +1,86 @@
 import React from 'react'
 import './CourseMaterialsView.css'
+import {
+    Collapse, Button, CardBody, Card, CardTitle, CardSubtitle, CardHeader,
+    CardText, ListGroup, ListGroupItem, CardLink
+} from 'reactstrap';
 
-export default class CourseMaterialsView extends React.Component{
+export default class CourseMaterialsView extends React.Component {
     constructor(props) {
         super(props);
-        
+        this.openBox = this.openBox.bind(this)
+
         this.state = {
-            courseMaterials = [
+            courseMaterials: [
                 {
                     course: "JavaScript",
-                    resource: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript", 
-                    tutorial: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps"
+                    url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/"
                 },
                 {
-                    course:"HTML",
-                    resource: "https://developer.mozilla.org/en-US/docs/Glossary/HTML#Concept_and_syntax",
-                    tutorial: "https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML"
+                    course: "HTML",
+                    url: "https://developer.mozilla.org/en-US/docs/Glossary/HTML#Concept_and_syntax/"
                 },
                 {
-                    course: "CSS", 
-                    resource: "https://developer.mozilla.org/en-US/docs/Glossary/CSS",
-                    tutorial: "https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/CSS_basics"
+                    course: "CSS",
+                    url: "https://developer.mozilla.org/en-US/docs/Glossary/CSS"
+                },
+                {
+                    course: "SQL",
+                    url: "https://www.khanacademy.org/computing/computer-programming/sql/"
                 }
-            ]  
+            ],
+            isOpen: false
         }
     }
 
-    render(){
-        return(
-           <div className="container-fluid">
+    openBox() {
+        this.setState({ isOpen: !this.state.isOpen })
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
                 <div className="row">
                     <div className="col-12">
-                        <h1>Course Materials</h1> 
-                    {this.state.courseMaterials.map(materials => <h1>{materials}</h1>) }
-                   </div>
+                        <h1>Course Materials</h1>
+                        {this.state.courseMaterials.map(materials =>
+
+                            <Card className="mb-2">
+                                <CardHeader className="custom-card-header" onClick={this.openBox}>
+                                    <div className="container-fluid">
+                                        <div className="row align-items-center">
+                                            <div className="col-11">
+
+                                                <h1>{materials.course}</h1>
+                                            </div>
+
+                                            <div className="col-1">
+                                                <i className="fas fa-angle-down fa-lg float-right"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </CardHeader>
+                                <Collapse isOpen={!this.state.isOpen}>
+                                    <CardBody>
+                                        <div className="mb-1 text-muted"><em>Resource</em></div>
+                                        <div><p> 
+                                    <CardLink className="d-block"><a href={materials.url} target="_blank">{materials.url}</a></CardLink>
+                                        </p></div>
+                                    </CardBody>
+                                </Collapse>
+
+                            </Card>
+
+                        )}
+                    </div>
                 </div>
-           </div>
+            </div>
         )
     }
 }
+
+
+//make each card independent dependant on topic
+//make the cards components but identify its unique aspects 
+// each card needs its own state = each the crds will need its own open/close function [independent]
