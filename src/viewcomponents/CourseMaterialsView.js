7d3227bb4a2f5d6b50b326dbb11604ca50badda9
TiffanyJan <tiffany_low@hotmail.com>
2019-03-04 18:48:10 +0000

    course material view changes

--- src/viewcomponents/CourseMaterialsView.js
+++ src/viewcomponents/CourseMaterialsView.js
@@ -62,13 +62,11 @@ export default class CourseMaterialsView extends React.Component {
                     currentWeek: false
                 },
                 {
-                    weekNumber: "Week 7 - React (Part 2)",
-                    slide: "https://docs.google.com/document/d/1xlceEDCEM5piVF07kteUoZNEcr1nrLu13YLXxmyXyrk/edit",
-                    lectureUrls: ["https://drive.google.com/file/d/1_hlktJFqvNBCBJiqryhL3gmxytZSvZvY/view?usp=sharing",
-                        "https://drive.google.com/file/d/1BOKC9hp2CLC4ZmCdxPzfnXhRj8PrDdC3/view?usp=sharing",
-                        "https://drive.google.com/file/d/1WjflDVJZVtU9Ax0X4k9enz547ouLTskK/view?usp=sharing",
-                        "https://drive.google.com/file/d/13Ueo0dlbQuru0UpqmELTksIND5zP2Ccl/view?usp=sharing",
-                        "https://drive.google.com/file/d/1PKoUG3nNilRioaDVxheHRqycmDc--0Ir/view?usp=sharing"],
+                    weekNumber: 7,
+                    slides: "https://docs.google.com/document/d/1xlceEDCEM5piVF07kteUoZNEcr1nrLu13YLXxmyXyrk/edit",
+                    topic: "React - Part 2",
+                    lectureWebinar: ["https://drive.google.com/file/d/1BOKC9hp2CLC4ZmCdxPzfnXhRj8PrDdC3/view?usp=sharing",
+                        "https://drive.google.com/file/d/1WjflDVJZVtU9Ax0X4k9enz547ouLTskK/view?usp=sharing"],
                     homeworkWebinar: ["https://drive.google.com/file/d/1_hlktJFqvNBCBJiqryhL3gmxytZSvZvY/view?usp=sharing",
                         "https://drive.google.com/file/d/13Ueo0dlbQuru0UpqmELTksIND5zP2Ccl/view?usp=sharing",
                         "https://drive.google.com/file/d/1PKoUG3nNilRioaDVxheHRqycmDc--0Ir/view?usp=sharing"],
@@ -91,9 +89,10 @@ export default class CourseMaterialsView extends React.Component {
                     currentWeek: false
                 },
                 {
-                    weekNumber: "Week 9 - Introduction to Databases",
-                    slide: "https://docs.google.com/document/d/17yVQ7OOU1JQhiueiDp-D1-9W_jeda6CI6A2nYd3JzE8/edit",
-                    lectureUrls: ["https://drive.google.com/file/d/1xmBcArEO6dM1kpT2oSTAuYcSYjrN-7_u/view?usp=sharing"],
+                    weekNumber: 9,
+                    slides: "https://docs.google.com/document/d/17yVQ7OOU1JQhiueiDp-D1-9W_jeda6CI6A2nYd3JzE8/edit",
+                    topic: "Introduction to Databases",
+                    lectureWebinar: ["https://drive.google.com/file/d/1xmBcArEO6dM1kpT2oSTAuYcSYjrN-7_u/view?usp=sharing"],
                     homeworkWebinar: ["https://drive.google.com/file/d/1H6iPLxSrWLjACwnou5DJAPfWa4mCCXb_/view?usp=sharing"],
                     homeworkTaskIntro: "Finish Front-end Application",
                     homeworkTaskList: [],
@@ -130,12 +129,6 @@ export default class CourseMaterialsView extends React.Component {
                     homeworkTaskIntro: "Connect the back-end to the front-end",
                     homeworkTaskList: [],
                     currentWeek: false
-                },
-                {
-                    weekNumber: "Week 13 - Career Development",
-                    slide: "https://docs.google.com/document/d/1lfoDOyZJFcdiga0xGMYgvaaPKHmAnHs9YnE2sMvyhCo/edit",
-                    lectureUrls: ["https://drive.google.com/file/d/1RoDOrLgx46R4B-_hWIRujfQp28tS9yka/view?usp=sharing"],
-                    homeworkWebinar:[]
                 }
             ]
         }
